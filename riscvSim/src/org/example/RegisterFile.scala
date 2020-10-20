fa844c509d75b7554225ed89ed2f676d37355cb8
Sofus Dawids <sofus.dawids@gmail.com>
2020-10-20 10:10:13 +0200

    Har simplificeret klassen RegisterFile og har tilføjet læse- og skrive-funktioner.

--- riscvSim/src/org/example/RegisterFile.scala
+++ riscvSim/src/org/example/RegisterFile.scala
@@ -1,5 +1,10 @@
 package org.example
 
-class RegisterFile(val data: Array[Int]) {
-    def this(size: Int) = this(new Array[Int](size))
+class RegisterFile(private val size: Int, private val offset: Int = 1) {
+    private val data = new Array[Int](size)
+
+    def readRegister(register: Int): Int = data(register - offset)
+    def writeRegister(register: Int, value: Int): Unit = {
+        data(register - offset) = value
+    }
 }
