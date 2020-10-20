3d908049bb282887dc41b9a6b81c57bf5caa2385
Sofus Dawids <sofus.dawids@gmail.com>
2020-10-20 10:48:54 +0200

    Har opdateret main-metoden.

--- riscvSim/src/org/example/Simulator.scala
+++ riscvSim/src/org/example/Simulator.scala
@@ -1,17 +1,34 @@
 package org.example
 
+import java.io._
+import scala.language.postfixOps
+
 object Simulator {
     def main(args: Array[String]): Unit = {
-        val memory = new Memory(32)
 
-        memory.writeByte(0, 0xff)
+        // Read from file into Byte Array.
+        // val bis = new BufferedInputStream(new FileInputStream("riscvSim/resources/test1.bin"))
+        // val byteArray = Stream.continually(bis.read).takeWhile(-1 !=).map(_.toByte).toArray
+
+        // Choose first argument as memory size and 1024 as default.
+        val memorySize = if (args.length > 0) {
+            args(0).toIntOption.getOrElse(0)
+        } else 1024
+
+        val memory = new Memory(memorySize)
+
+        // Test reads and writes
+        memory.writeByte(0, 0xef)
+        memory.writeByte(1, 0xbe)
+        memory.writeByte(2, 0xad)
+        memory.writeByte(3, 0xde)
         memory.writeHalfword(4, 0xdead)
         memory.writeWord(8, 0xdeadbeef)
 
         println(memory.readByte(0).toHexString)
         println(memory.readByteUnsigned(0).toHexString)
-        println(memory.readHalfword(4).toHexString)
+        println(memory.readHalfword(0).toHexString)
         println(memory.readHalfwordUnsigned(4).toHexString)
-        println(memory.readWord(8).toHexString)
+        println(memory.readWord(0).toHexString)
     }
 }
