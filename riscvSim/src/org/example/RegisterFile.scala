569a7b334891023aebe9a250f08de9c9d4323e2d
Sofus Dawids <sofus.dawids@gmail.com>
2020-11-03 21:59:38 +0100

    Rettet størrelsen af Registerfile.

--- riscvSim/src/org/example/RegisterFile.scala
+++ riscvSim/src/org/example/RegisterFile.scala
@@ -1,7 +1,7 @@
 package org.example
 
 class RegisterFile(private val size: Int, private val offset: Int = 1) {
-    private val data = new Array[Int](size)
+    private val data = new Array[Int](size - offset)
 
     def readRegister(register: Int): Int = data(register - offset)
     def writeRegister(register: Int, value: Int): Unit = {
