ee3bb3ba23f8b5e353693dc5185d58a1319079fd
Sofus Dawids <sofus.dawids@gmail.com>
2020-11-10 16:46:28 +0100

    Har ændret readRegister og writeRegister til apply og update

--- riscvSim/src/org/example/RegisterFile.scala
+++ riscvSim/src/org/example/RegisterFile.scala
@@ -3,8 +3,16 @@ package org.example
 class RegisterFile(private val size: Int, private val offset: Int = 1) {
     private val data = new Array[Int](size - offset)
 
-    def readRegister(register: Int): Int = data(register - offset)
-    def writeRegister(register: Int, value: Int): Unit = {
+    def apply(register: Int): Int = if (register > 0) data(register - offset) else 0
+    def update(register: Int, value: Int): Unit = {
         data(register - offset) = value
     }
+
+    def printRegisters(): Unit = {
+        for (i <- 0 until size - offset) {
+            println(f"x${i + offset}%02d: ${data(i)}")
+        }
+    }
+
+
 }
